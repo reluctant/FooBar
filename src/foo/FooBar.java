18620210627807f93a556c6c2a445ff0422b823b
Tony Li <reluctant1981@gmail.com>
2015-08-10 19:10:01 +0900

    Add a judgement.

--- src/foo/FooBar.java
+++ src/foo/FooBar.java
@@ -2,6 +2,9 @@ package foo;
 
 pubic class FooBar {
     public static void main(String[] args) {
+        if (args.length < 1) {
+            return;
+        }
         System.out.println(args.length);
         System.out.println("FooBar");
     }
