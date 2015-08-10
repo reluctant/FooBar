389d0de880557a7f21a2508fba94156b52031bc3
Tony Li <reluctant1981@gmail.com>
2015-08-10 19:19:46 +0900

    Add a inner static class.

--- src/foo/FooBar.java
+++ src/foo/FooBar.java
@@ -1,6 +1,9 @@
 package foo;
 
 pubic class FooBar {
+    private static class FooBarChild {
+        private String name;
+    }
     public static void main(String[] args) {
         System.out.println(args.length);
         System.out.println("FooBar");
