77215c50e7284ddeafbce2d5e4751584a8f55177
Tony Li <reluctant1981@gmail.com>
2015-08-10 19:29:26 +0900

    Add a second inner static class.

--- src/foo/FooBar.java
+++ src/foo/FooBar.java
@@ -4,6 +4,9 @@ pubic class FooBar {
     private static class FooBarChild {
         private String name;
     }
+    private static class FooBarChild2 {
+        private int age;
+    }
     public static void main(String[] args) {
         if (args.length < 1) {
             return;
